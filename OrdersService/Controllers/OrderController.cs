3b37c8c24d09d39b9f11f463113a558c98d0d988
sunilsmail <sunilsmail.amujuri@gmail.com>
2021-01-04 23:14:56 +0530

    init

---
+++ OrdersService/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrdersService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Order1", "Order2" };
+        }
+    }
+}
