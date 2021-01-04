427805f7ee39730198d9539b5ded6bbeff69c50e
sunilsmail <sunilsmail.amujuri@gmail.com>
2021-01-04 23:13:18 +0530

    Add files via upload

---
+++ UsersService/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UsersService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "User1", "User2" };
+        }
+    }
+}
